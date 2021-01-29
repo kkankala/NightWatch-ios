157722c1b14f207048723d4d3b017bff3314e33b
Kalyan Kankala <kankala.kalyan@gmail.com>
2021-01-28 22:04:34 -0500

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Kalyan Kankala on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
