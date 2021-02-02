cdb7fdbab2d081cce5ea30f1c7a4976ec58c4ce0
Kalyan Kankala <kankala.kalyan@gmail.com>
2021-02-01 22:15:52 -0500

    single page app list view

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -27,101 +27,38 @@ let monthlyTasks = [
     "Test motion detectors",
     "Test smoke alarms"
 ]
-//struct ContentView: View {
-//    var body: some View {
-//        List{
-//            Section(header:HStack{
-//                Image(systemName: "moon.stars")
-//                Text("Nightly Tasks")
-//            }.font(.title3)){
-//                ForEach(nightlyTasks,id:\.self,content:{
-//                    taskName in Text(taskName)
-//                })
-//            }
-//            Section(header:HStack{
-//                Image(systemName: "sunset")
-//                Text("Weekly Tasks")
-//            }.font(.title3)){
-//                ForEach(weeklyTasks,id:\.self,content:{
-//                    taskName in Text(taskName)
-//                })
-//            }
-//            Section(header:HStack{
-//                Image(systemName: "calendar")
-//                Text("Monthly Tasks")
-//            }.font(.title3)){
-//                ForEach(monthlyTasks,id:\.self,content:{
-//                    taskName in Text(taskName)
-//                })
-//            }
-//        }.listStyle(GroupedListStyle())
-//    }
-//}
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack{
-                VStack(alignment: .leading){
-                    // MARK: Nightly Task
-                    Group{
-                        HStack {
-                            Text(Image(systemName:"moon.stars"))
-                                .foregroundColor(.yellow)
-                                .font(.title)
-                                .fontWeight(.heavy)
-                            Text("Nightly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
-                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        }
-                        Text("Check that the safe is locked")
-                    }
-                    // MARK: Weekly Tasks
-                    Group {
-                        Divider()
-                        HStack {
-                            Text(Image(systemName:"sunset"))
-                                .foregroundColor(.yellow)
-                                .font(.title)
-                                .fontWeight(.heavy)
-                            Text("Weekly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
-                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        }
-                        Text("Check that the safe is locked")
-                    }
-                    // MARK: Monthly Tasks
-                    Group {
-                        Divider()
-                        HStack {
-                            Text(Image(systemName:"calendar"))
-                                .foregroundColor(.yellow)
-                                .font(.title)
-                                .fontWeight(.heavy)
-                            Text("Monthly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(.heavy)
-                                .foregroundColor(.yellow)
-                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        }
-                        Text("Check that the safe is locked")
-                    }
-                }
-                .foregroundColor(.gray)
-                Spacer()
+        List{
+            Section(header:HStack{
+                Image(systemName: "moon.stars")
+                Text("Nightly Tasks")
+            }.font(.title3)){
+                ForEach(nightlyTasks,id:\.self,content:{
+                    taskName in Text(taskName)
+                })
             }
-            .padding([.top,.leading])
-            Spacer()
-        }
+            Section(header:HStack{
+                Image(systemName: "sunset")
+                Text("Weekly Tasks")
+            }.font(.title3)){
+                ForEach(weeklyTasks,id:\.self,content:{
+                    taskName in Text(taskName)
+                })
+            }
+            Section(header:HStack{
+                Image(systemName: "calendar")
+                Text("Monthly Tasks")
+            }.font(.title3)){
+                ForEach(monthlyTasks,id:\.self,content:{
+                    taskName in Text(taskName)
+                })
+            }
+        }.listStyle(GroupedListStyle())
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
